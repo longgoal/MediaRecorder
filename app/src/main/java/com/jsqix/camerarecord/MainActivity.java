c45d226716b4fbb04fc791dbb4e9fe2245d267f9
liuxiyu <liuxiyu@hotmail.com>
2020-07-13 17:43:09 +0800

    调试支持960*288分辨率 preview，picture，video 的特殊监控摄像头的预览界面。 --960*288 是每次传一般高度的图像，所需预览界面要是288的2倍 --拍照和录像文件的尺寸都是960*288，所以保存下来的图片和视频都是变形的，高度压缩到正常的1/2了。

--- app/src/main/java/com/jsqix/camerarecord/MainActivity.java
+++ app/src/main/java/com/jsqix/camerarecord/MainActivity.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -19,7 +21,7 @@ import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private SurfaceView mCameraPreview;
+    private ResizeAbleSurfaceView mCameraPreview;
     private SurfaceHolder mSurfaceHolder;
     private ImageButton mShutter;
     private TextView mMinutePrefix;
@@ -51,10 +53,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
+
         setContentView(R.layout.activity_main);
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, TAG);
-        mCameraPreview = (SurfaceView) findViewById(R.id.camera_preview);
+        mCameraPreview = (ResizeAbleSurfaceView) findViewById(R.id.camera_preview);
+        mCameraPreview.resize(960,576);
         mMinutePrefix = (TextView) findViewById(R.id.timestamp_minute_prefix);
         mMinuteText = (TextView) findViewById(R.id.timestamp_minute_text);
         mSecondPrefix = (TextView) findViewById(R.id.timestamp_second_prefix);
@@ -121,12 +137,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (size != null) {
             parameters.setPreviewSize(size.width, size.height);
         }
-
+        parameters.setPreviewSize(960, 288);
         //parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
         parameters.setPreviewFrameRate(20);
 
         //设置相机预览方向
-        mCamera.setDisplayOrientation(90);
+        mCamera.setDisplayOrientation(0);
 
         mCamera.setParameters(parameters);
 
@@ -193,7 +209,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mCamera.unlock();
         //给Recorder设置Camera对象，保证录像跟预览的方向保持一致
         mRecorder.setCamera(mCamera);
-        mRecorder.setOrientationHint(90);  //改变保存后的视频文件播放时是否横屏(不加这句，视频文件播放的时候角度是反的)
+        mRecorder.setOrientationHint(0);  //改变保存后的视频文件播放时是否横屏(不加这句，视频文件播放的时候角度是反的)
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC); // 设置从麦克风采集声音
         mRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA); // 设置从摄像头采集图像
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);  // 设置视频的输出格式 为MP4
@@ -201,7 +217,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264); // 设置视频的编码格式
         mRecorder.setVideoEncodingBitRate(3 * 1024 * 1024);// 设置视频编码的比特率
         //mRecorder.setVideoSize(1280, 720);  // 设置视频大小
-        mRecorder.setVideoSize(176, 144);  // 设置视频大小
+        mRecorder.setVideoSize(960, 288);  // 设置视频大小
         mRecorder.setVideoFrameRate(20); // 设置帧率
 //        mRecorder.setMaxDuration(10000); //设置最大录像时间为10s
         mRecorder.setMaxDuration(MAX_DURATION);
